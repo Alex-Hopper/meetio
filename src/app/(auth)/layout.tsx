88231ed9830455ce3ae83610f410bedc7a94a12b
Alex Hopper <hopperdalex@gmail.com>
2026-04-01 01:50:25 -0700

    add logo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,10 +8,14 @@ export default function AuthLayout({
   return (
     <div className="flex flex-1">
       {/* Left — branding panel */}
-      <div className="hidden lg:flex lg:w-1/2 flex-col justify-center p-12 bg-linear-to-br from-violet-400 via-purple-500 to-indigo-600 text-white">
+      <div className="hidden lg:flex lg:w-1/2 flex-col justify-center p-12 bg-linear-to-br from-blue-400 via-blue-500 to-indigo-600 text-white">
         <div className="flex items-center gap-2 text-lg font-semibold tracking-tight">
-          <div className="size-7 rounded-full bg-white/20 flex items-center justify-center">
-            <div className="size-3 rounded-full bg-white" />
+          <div className="size-8 rounded-full bg-white/50 flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 1000 1000" fill="none">
+                <path d="M629.544 166.863C645.75 143.304 680.724 143.865 696.165 167.931L953.026 568.248C970.107 594.87 950.991 629.849 919.36 629.849L771.577 629.849C757.706 629.849 744.825 622.663 737.539 610.86L555.713 316.299C547.387 302.811 547.811 285.678 556.795 272.619L629.544 166.863Z" fill="#1447E6"/>
+                <path d="M306.193 178.172C320.955 151.314 359.198 150.354 375.29 176.436L687.65 682.736C695.708 695.797 695.583 712.316 687.328 725.253L617.921 834.037C602.068 858.883 565.702 858.635 550.189 833.575L242.425 336.412C234.831 324.144 234.432 308.737 241.382 296.092L306.193 178.172Z" fill="#1447E6"/>
+                <path d="M140.518 412.477C156.89 389.258 191.572 390.032 206.893 413.956L384.799 691.763C393.915 705.998 393.099 724.427 382.76 737.8L308.306 834.107C291.183 856.255 257.19 854.415 242.558 830.548L71.7589 551.943C63.4093 538.324 63.9643 521.042 73.1706 507.986L140.518 412.477Z" fill="#1447E6"/>
+              </svg>
           </div>
           Meetio
         </div>
